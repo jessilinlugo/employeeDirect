e08a1e575471229444fce58aafb6c591845ca53f
jessilin <jessilinlugo@gmail.com>
2021-03-28 23:27:31 -0700

    connected the API and displayed randomized results

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import EmployeeCard from "./components/Employee"
+import axios from "axios";
 
 function App() {
+  const [employeeState, setEmployeeState] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://randomuser.me/api/?results=25&nat=us")
+    .then(allUsers => {
+      setEmployeeState(allUsers.data.results.map(result => {
+        let employeeData = {
+          firstName: result.name.first,
+          lastName: result.name.last,
+          email: result.email,
+          state: result.location.state,
+          age: result.dob.age,
+          gender: result.gender,
+          id: result.login.uuid
+        }
+
+        return employeeData;
+      }))
+    })
+  }, [])
+
   return (
-    <Router>
       <div>
         <Navbar />
         <Wrapper>
-          <EmployeeCard/>
+<div className="container mt-4">
+<table className="table">
+  <thead>
+    <tr>
+      <th scope="col-2">First</th>
+      <th scope="col-2">Last</th>
+      <th scope="col-2">Email</th>
+      <th scope="col-2">State</th>
+      <th scope="col-2">Age</th>
+      <th scope="col-2">Gender</th>
+    </tr>
+  </thead>
+  <tbody>
+    {employeeState.map(employee => <EmployeeCard
+      firstName={employee.firstName}
+      lastName={employee.lastName}
+      email={employee.email}
+      state={employee.state}
+      age={employee.age}
+      gender={employee.gender}
+      />
+      )}
+  </tbody>
+</table>
+</div>
         </Wrapper>
         <Footer />
       </div>
-    </Router>
   );
 }
 
--- src/components/Employee/index.js
+++ src/components/Employee/index.js
@@ -1,28 +1,18 @@
 import React from "react";
-import "./style.css";
 
-
-function EmployeeCard(props) {
+function Employee({firstName, lastName, email, state, age, gender}) {
   return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div>
-    </div>
+<div>
+  <tr>
+    <td className="first-name" key="1">{firstName}</td>
+    <td className="last-name" key="2">{lastName}</td>
+    <td className="email" key="3">{email}</td>
+    <td className="state" key="4">{state}</td>
+    <td className="age" key="5">{age}</td>
+    <td className="gender" key="6">{gender}</td>
+  </tr>
+</div>
   );
 }
 
-export default EmployeeCard;
+export default Employee;
