6418ef58942da4000125d7ed65cb8f510ded0bd8
jessilin <jessilinlugo@gmail.com>
2021-03-29 20:56:38 -0700

    changed the returned info and removed the div that was messing with my styling

--- src/components/Employee/index.js
+++ src/components/Employee/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-function Employee({firstName, lastName, email, state, age, gender}) {
+function Employee({firstName, lastName, email, city, state, age}) {
   return (
-<div>
+<>
   <tr>
     <td className="first-name" key="1">{firstName}</td>
     <td className="last-name" key="2">{lastName}</td>
     <td className="email" key="3">{email}</td>
-    <td className="state" key="4">{state}</td>
-    <td className="age" key="5">{age}</td>
-    <td className="gender" key="6">{gender}</td>
+    <td className="city" key="4">{city}</td>
+    <td className="state" key="5">{state}</td>
+    <td className="age" key="6">{age}</td>
+  
   </tr>
-</div>
+</>
   );
 }
 
